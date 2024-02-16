fcbd11114b2ec647b3f8e8537495cea95bcc5f2f
Martin Mouritzen <martin@martinmouritzen.dk>
2024-02-16 12:07:43 +0100

    web should not launch electron

--- vite.config.js
+++ vite.config.js
@@ -4,33 +4,42 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
+import vitePluginSvgr from 'vite-plugin-svgr'
 import pkg from './package.json'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+let defaultConfig = {
+	plugins: [
+		vitePluginSvgr(),
+		react(),
+	],
+	resolve: {
+		alias: {
+			'@': path.join(__dirname,'src')
+		}
+	}
+};
+
+export default defineConfig(({ mode }) => {
 	rmSync('dist-electron', { recursive: true, force: true })
 
-	const isServe = command === 'serve'
-	const isBuild = command === 'build'
+	const isServe = mode === 'serve'
+	const isBuild = mode === 'build'
 	const sourcemap = isServe || !!process.env.VSCODE_DEBUG
 
-	return {
-		resolve: {
-			alias: {
-				'@': path.join(__dirname, 'src')
-			},
-		},
-		plugins: [
-			react(),
+	if (mode === 'web') {
+		return defaultConfig;
+	}
+	else {
+		defaultConfig.plugins = [
+			...defaultConfig.plugins,
 			electron([
 				{
 					// Main-Process entry file of the Electron App.
 					entry: 'electron/main/index.js',
 					onstart(options) {
 						if (process.env.VSCODE_DEBUG) {
-							console.log(/* For `.vscode/.debug.script.mjs` */'[startup] Electron App')
-						}
-						else {
+							console.log('[startup] Electron App')
+						} else {
 							options.startup()
 						}
 					},
@@ -66,14 +75,15 @@ export default defineConfig(({ command }) => {
 			]),
 			// Use Node.js API in the Renderer-process
 			renderer(),
-		],
-		server: process.env.VSCODE_DEBUG && (() => {
-			const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
-			return {
-				host: url.hostname,
-				port: +url.port,
-			}
+		];
+		defaultConfig.server = process.env.VSCODE_DEBUG && (() => {
+				const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
+				return {
+					host: url.hostname,
+					port: +url.port,
+				}
 		})(),
-		clearScreen: false,
+		defaultConfig.clearScreen = false;
+		return defaultConfig;
 	}
 })
